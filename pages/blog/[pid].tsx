39fa9ac256750bd9172b4fbc101783c3bd719372
lino-levan <11367844+lino-levan@users.noreply.github.com>
2022-03-26 19:49:50 -0700

    Change page title to be post title

--- pages/blog/[pid].tsx
+++ pages/blog/[pid].tsx
@@ -19,7 +19,7 @@ const Post: NextPage = (props: any) => {
   return (
     <div>
       <Head>
-        <title>Lino Le Van</title>
+        <title>{post.title}</title>
         <meta name="description" content={post.post.split("\n")[0]} />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
